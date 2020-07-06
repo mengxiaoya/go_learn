0d65395ca0455c5171ee55b90e8b01420b832fb4
bonree.zq <zhangqi@bonree.com>
2020-07-06 18:20:22 +0800

    init

--- chapter2/sample/main.go
+++ chapter2/sample/main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"os"
 
+	// _ 的作用是：让Go语言对包做初始化操作，但是并不使用包里的标识符
+	// 因为为了让程序的可读性更强，Go编译器不允许声明导入某个包却不使用
 	_ "github.com/goinaction/code/chapter2/sample/matchers"
 	"github.com/goinaction/code/chapter2/sample/search"
 )
