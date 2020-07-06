d14647946292093b332739e7b40b417671a8cadf
bonree.zq <zhangqi@bonree.com>
2020-07-06 18:35:48 +0800

    add comments

--- chapter2/sample/main.go
+++ chapter2/sample/main.go
@@ -5,14 +5,15 @@ import (
 	"os"
 
 	// _ 的作用是：让Go语言对包做初始化操作，但是并不使用包里的标识符
-	// 因为为了让程序的可读性更强，Go编译器不允许声明导入某个包却不使用
+	// 因为为了让程序的可读性更强，Go编译器不允许声明导入某个包却不使用，所以这个下划线让编译器接收这类导入
+	// 并且调用对应包内的所有代码文件里定义的init函数，对应到这个的话就是调用matchers包下的rss.go中的init函数
 	_ "github.com/goinaction/code/chapter2/sample/matchers"
 	"github.com/goinaction/code/chapter2/sample/search"
 )
 
-// init is called prior to main.
+// init函数在main函数之前调用
 func init() {
-	// Change the device for logging to stdout.
+	// 下面这行代码的作用是将标准库里日志类的输出从默认的标准错误stderr设置为标准输出stdout
 	log.SetOutput(os.Stdout)
 }
 
