0fd6d8c8b8ed2fd5ab6d18ae5a57bb136c0fa52c
bonree.zq <zhangqi@bonree.com>
2020-07-10 11:43:58 +0800

    add comments

--- chapter2/sample/search/default.go
+++ chapter2/sample/search/default.go
@@ -30,3 +30,28 @@ func init() {
 func (m defaultMatcher) Search(feed *Feed, searchTerm string) ([]*Result, error) {
 	return nil, nil
 }
+
+// 与直接通过值或者指针调用方法不同，如果通过接口类型的值调用方法，规则有很大不同
+// 例如下面的代码段1 和代码段2
+
+// 代码段1
+// 方法声明为使用指向 defaultMatcher 类型值的指针作为接收者
+// func (m *defaultMatcher) Search(feed *Feed, searchTerm string)
+// 通过 interface 类型的值来调用方法
+// var dm defaultMatcher
+// var matcher Matcher = dm     // 将值赋值给接口类型
+// matcher.Search(feed, "test") // 使用值来调用接口方法
+
+// 代码段2
+// 方法声明为使用 defaultMatcher 类型的值作为接收者
+// func (m defaultMatcher) Search(feed *Feed, searchTerm string)
+// 通过 interface 类型的值来调用方法
+// var dm defaultMatcher
+// var matcher Matcher = &dm // 将指针赋值给接口类型
+// matcher.Search(feed, "test") // 使用指针来调用接口方法
+
+// 代码段1执行go build编译的时候会报错：
+// cannot use dm (type defaultMatcher) as type Matcher in assignment
+// 而代码段2可以正常编译，这是因为
+// 使用指针作为接收者声明的方法，只能在接口类型的值是一个指针的时候被调用。
+// 使用值作为接收者声明的方法，在接口类型的值为值或者指针时，都可以被调用。
